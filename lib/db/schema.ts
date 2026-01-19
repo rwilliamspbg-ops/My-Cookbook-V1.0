9f2764c6e2ce4f1319374834cfd1e16964e8125c
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-19 10:57:45 -0800

    Update schema.ts

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -8,7 +8,10 @@ export const recipes = sqliteTable('recipes', {
   ingredients: text('ingredients'),
   instructions: text('instructions'), 
   servings: integer('servings'),         
-  imageUrl: text('image_url'),        
+  imageUrl: text('image_url'),
+  prep_time: text('prep_time'), 
+  cook_time: text('cook_time'),
+  
 });
 
 export const collections = sqliteTable('collections', {
