94f537caaccd45f2454e7d7a36ac18b829430b74
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-19 09:45:00 -0800

    Update route.ts

--- app/api/collections/route.ts
+++ app/api/collections/route.ts
@@ -1,5 +1,5 @@
-import { db } from '@/lib/db';
-import { collections } from '@/lib/db/schema';
+import { db } from '../../../lib/db';
+import { collections } from '../../../lib/db/schema';
 import { eq } from 'drizzle-orm';
 
 export async function GET() {
