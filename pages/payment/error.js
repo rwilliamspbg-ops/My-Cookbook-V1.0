fd2cf1f157a055d835a021b91174224f7c8f49f3
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-20 12:16:22 -0800

    Create error.js

---
+++ pages/payment/error.js
@@ -0,0 +1,14 @@
+// pages/payment/error.js
+import Link from "next/link";
+
+export default function PaymentError() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Payment Error</h1>
+      <p>There was a problem processing your payment. Please try again.</p>
+      <Link href="/pricing">
+        <button>Try Again</button>
+      </Link>
+    </main>
+  );
+}
