ce7d5ee30bf9980b8e2e9da178e7c887166fa959
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-20 09:08:20 -0800

    Add Pricing link to navigation

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -7,11 +7,13 @@ export default function AppLayout({ children }) {
       <header className="app-header">
         <div className="app-header-inner">
           <Link href="/" className="app-logo">
-            🍳 My Cookbook
+            My Cookbook
           </Link>
           <nav className="app-nav">
+            <Link href="/">Recipes</Link>
             <Link href="/upload">Parse Recipe</Link>
             <Link href="/recipe/new">Add Recipe</Link>
+            <Link href="/pricing">Pricing</Link>
           </nav>
         </div>
       </header>
