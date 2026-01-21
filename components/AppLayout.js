5326f3d08899ad6916b01237b0823e5c0ea77760
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 08:36:54 -0800

    Update AppLayout.js

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,23 +1,3 @@
 // components/AppLayout.js
 import Link from 'next/link';
-
-export default function AppLayout({ children }) {
-  return (
-    <div className="app-shell">
-      <header className="app-header">
-        <div className="app-header-inner">
-          <Link href="/" className="app-logo">
-            My Cookbook
-          </Link>
-          <nav className="app-nav">
-            <Link href="/">Recipes</Link>
-            <Link href="/upload">Parse Recipe</Link>
-            <Link href="/recipe/new">Add Recipe</Link>
-            <Link href="/pricing">Pricing</Link>
-          </nav>
-        </div>
-      </header>
-      <main className="app-main">{children}</main>
-    </div>
-  );
-}
+import {
