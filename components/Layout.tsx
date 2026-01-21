30340b63df512b5ac7eb670af0b41d2ffd0866fb
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 07:37:24 -0800

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,6 @@
-// components/Layout.tsx
-import React from "react";
-import Link from "next/link";
+import Head from "next/head";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="app-root">
-
-      <main className="app-main">{children}</main>
-      <footer className="app-footer">
-        <span>© {new Date().getFullYear()} My Cookbook</span>
-      </footer>
-    </div>
-  );
+type LayoutProps = {
+  children: React.ReactNode;
+};
 }
