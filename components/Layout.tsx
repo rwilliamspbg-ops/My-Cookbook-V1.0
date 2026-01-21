8dace2827c882d5f86a4175b0e1bdb28dbff05dd
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 07:34:45 -0800

    Update Layout.tsx
    removed header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,21 +5,7 @@ import Link from "next/link";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="app-root">
-      <header className="app-header">
-        <div className="app-header-inner">
-          <Link href="/" className="brand">
-            <span className="brand-icon">🍳</span>
-            <span className="brand-name">My Cookbook</span>
-            <span className="brand-version">V1.0</span>
-          </Link>
-          <nav className="app-nav">
-            <Link href="/recipes">Recipes</Link>
-            <Link href="/upload">Parse Recipe</Link>
-            <Link href="/recipe/new">Add Recipe</Link>
-            <Link href="/pricing">Pricing</Link>
-          </nav>
-        </div>
-      </header>
+
       <main className="app-main">{children}</main>
       <footer className="app-footer">
         <span>© {new Date().getFullYear()} My Cookbook</span>
