ed868fc03d581ea16bf53a877895762402171c67
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 12:48:58 -0800

    Fix ingredient label styling by using className instead of inline style
    - Replace problematic inline style attribute with className
    - Use .ingredient-label class from globals.css for styling
    - Fixes React error about invalid object as child

--- pages/recipe/[id].js
+++ pages/recipe/[id].js
@@ -122,7 +122,7 @@ export default function RecipePage() {
                     id={`ingredient-${idx}`}
                     className="no-print"
                   />
-                  <label htmlFor={`ingredient-${idx}`}> style={{ color: '#000' }}{ingredient}</label>
+                  <label htmlFor={`ingredient-${idx}`}> className="ingredient-label"{ingredient}</label>
                 </li>
               ))}
             </ul>
