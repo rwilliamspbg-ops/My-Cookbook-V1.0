eaf71c1a1905e8e259f859bd5cc2354322b04123
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 10:45:45 -0800

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
+import AppLayout from "./AppLayout";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -17,28 +17,8 @@ export default function Layout({ children }: LayoutProps) {
         />
       </Head>
 
-      <div className="app-shell">
-        <header className="site-header">
-          <div className="site-header-inner">
-            <div className="site-logo">
-              <span role="img" aria-label="cookbook">
-                🔍
-              </span>
-              <span className="site-title">My Cookbook</span>
-              <span className="site-version">V1.0</span>
-            </div>
-
-            <nav className="site-nav">
-              <Link href="/recipes" className="nav-link">
-                Recipe Rolodex
-              </Link>
-              
-            </nav>
-          </div>
-        </header>
-
-        <main className="site-main">{children}</main>
-      </div>
+      <AppLayout>{children}</AppLayout>
     </>
   );
 }
+
