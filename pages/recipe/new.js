a0acd7a9c4e00230b9084ec2e491bf86a018425b
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-21 16:21:42 -0800

    Add AppLayout import to new.js

--- pages/recipe/new.js
+++ pages/recipe/new.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import Link from 'next/link';
+import AppLayout from '../../components/AppLayout';
 
 export default function RecipeForm({ _mode = "create", _initialValues }) {
   const router = useRouter();
