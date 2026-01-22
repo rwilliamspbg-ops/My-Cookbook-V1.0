2088b5b490f922e3846475c22dcf7c4ca5a77997
rwilliamspbg-ops <r.williamspbg@gmail.com>
2026-01-22 07:01:55 -0800

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,6 @@
+import Head from 'next/head';
+import Link from 'next/link'; // ← add this line
+import AppLayout from '../../components/AppLayout';
 <Link href="/admin/members" className="btn btn-primary">
   Open membership console
 </Link>
